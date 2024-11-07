59184cc005d23d8d6a3450f66e3bc6215dfcc694
xyl <416319808@qq.com>
2024-11-07 23:16:34 +0800

    server

--- server.go
+++ server.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
 	"grpcpro/pbfiles"
 	"grpcpro/services"
-	"net"
+	"net/http"
 )
 
 func main() {
+
+	//creds, err := credentials.NewServerTLSFromFile("keys/server.crt", "keys/server_no_passwd.key")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//rpcServer:=grpc.NewServer(grpc.Creds(creds))
 	rpcServer := grpc.NewServer()
 	pbfiles.RegisterProdServiceServer(rpcServer, new(services.ProdService))
 
-	lis, _ := net.Listen("tcp", ":8081")
+	//lis, _ := net.Listen("tcp", ":8081")
+	//rpcServer.Serve(lis)
 
-	rpcServer.Serve(lis)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 
+		fmt.Println(request)
+		rpcServer.ServeHTTP(writer, request)
+	})
+	httpServer := &http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+	}
+	httpServer.ListenAndServe()
+	//httpServer.ListenAndServeTLS("keys/server.crt", "keys/server_no_passwd.key")
 }
